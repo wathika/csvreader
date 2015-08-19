edfe0e40f26e631d88a361585af3d86db9d2bc50
wathika <vcwathika@gmail.com>
2015-08-19 20:13:13 +0300

    csv read when a string

--- read.rb
+++ read.rb
@@ -4,3 +4,16 @@ require 'csv'
 CSV.foreach('customers.csv') do |row|
   puts row.inspect
 end
+
+# if you have comma-separated data as a String object in Ruby, you can convert
+# the data to the Ruby representation of CSV (where the table is an array
+# containing other arrays i.e. the rows) with the CSV.parse method:
+a_string = "Dan,34\nMaria,55"
+CSV.parse(a_string) #=> [["Dan", "34"], ["Maria", "55"]]
+
+#you can also provide a block to CSV.parse
+CSV.parse(a_string) { |row| puts row.inspect }
+#=> produces ["Dan", "34"] and ["Maria", "55"] on separate lines
+
+#CSV.parse (without a block) === CSV.read interms of output
+#CSV.parse (with a block) === CSV.foreach interms of output
